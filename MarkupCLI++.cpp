70849c416583a14fa650d25a7873d9fcec5261e6
Nahida <szymon.cyb13@gmail.com>
2026-09-19 22:08:57 +0200

    Create int main with input file

---
+++ MarkupCLI++.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "MarkupCLI++ working!" << std::endl;
+    std::cout << "input file: " << argv[1] << std::endl;
+    return 0;
+}
