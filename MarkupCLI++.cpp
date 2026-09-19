facfc0cfae41d08472ef3dde64873a5d04bb53e5
Nahida <szymon.cyb13@gmail.com>
2026-09-19 22:31:51 +0200

    Require file name and header content

--- MarkupCLI++.cpp
+++ MarkupCLI++.cpp
@@ -7,11 +7,17 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    if (argc < 3) {
+        std::cout << "Error: Provide the file name and the header content." << std::endl;
+        return 1;
+    }
+
     std::ofstream file(argv[1]);
-    file << "# MarkupCLI++" << std::endl;
+    file << "# " << argv[2] << std::endl;
     file.close();
 
-    std::cout << "MarkupCLI++ working!" << std::endl;
+    std::cout << "Saved to " << argv[1] << std::endl;
+
     std::cout << "input file: " << argv[1] << std::endl;
     return 0;
 }
