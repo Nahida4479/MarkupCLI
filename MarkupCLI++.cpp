28fc761bcef5dcf18cc80a04d4197cae80d0a008
Nahida <szymon.cyb13@gmail.com>
2026-09-19 22:16:19 +0200

    Create files using fstream

--- MarkupCLI++.cpp
+++ MarkupCLI++.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Input file not found" << std::endl;
+        return 1;
+    }
+
+    std::ofstream file(argv[1]);
+    file << "# MarkupCLI++" << std::endl;
+    file.close();
+
     std::cout << "MarkupCLI++ working!" << std::endl;
     std::cout << "input file: " << argv[1] << std::endl;
     return 0;
